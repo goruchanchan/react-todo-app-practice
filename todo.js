d0bf10f71360bcb084781b35b5dd93d3890fdf80
goruchan <ppcfr481@yahoo.co.jp>
2023-06-03 22:21:12 +0900

    update 静的TodoList作成

--- todo.js
+++ todo.js
@@ -1,1 +1,27 @@
-// Write your code
+const todoList = ["hoge","foo"];
+const listItems = todoList.map((todo) =>
+  <li>{todo}</li>
+);
+
+function ToDoApp(props) {
+  return (
+    <div className="ToDoApp">
+      <h1>ToDoアプリ</h1>
+
+      <h2>ToDo追加</h2>
+      <div className="AddTodo"></div>
+
+      <h2>ToDo一覧</h2>
+
+      <ul>{props.test}</ul>
+
+      <div className="TodoTable">
+        <div className="ToDoRaw"></div>
+      </div>
+    </div>
+  );
+}
+
+const todoApp = ReactDOM.createRoot(document.getElementById("todoList"));
+todoApp.render(<ToDoApp todoItems={todoList.todoItems} test={listItems} />);
+
