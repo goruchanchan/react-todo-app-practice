52320c83d45696de772e89cd5a80b224655ec0ff
goruchan <ppcfr481@yahoo.co.jp>
2023-06-04 23:05:36 +0900

    clean 関数コンポーネントからクラスコンポーネントに変更

--- todo.js
+++ todo.js
@@ -1,26 +1,28 @@
-function ToDoApp(props) {
-  return (
-    <div className="ToDoApp">
-      <h1>ToDoアプリ</h1>
+class ToDoApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="ToDoApp">
+        <h1>ToDoアプリ</h1>
 
-      <h2>ToDo追加</h2>
-      <div className="AddTodo"></div>
+        <h2>ToDo追加</h2>
+        <div className="AddTodo"></div>
 
-      <h2>ToDo一覧</h2>
+        <h2>ToDo一覧</h2>
 
-      <ul>{props.todoItems}</ul>
+        <ul>{this.props.todoItems}</ul>
 
-      <div className="TodoTable">
-        <div className="ToDoRaw"></div>
+        <div className="TodoTable">
+          <div className="ToDoRaw"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-const listItems = ["hoge","foo"].map((todo) =>
-  <li key={todo}>{todo}</li>
-);
+const listItems = ["hoge", "foo"].map((todo) => <li key={todo}>{todo}</li>);
 
 const todoApp = ReactDOM.createRoot(document.getElementById("todoList"));
 todoApp.render(<ToDoApp todoItems={listItems} />);
-
