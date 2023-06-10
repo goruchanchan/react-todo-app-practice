e58a158d5ead6a079f7dd0842d50c057c10d7ef1
goruchan <ppcfr481@yahoo.co.jp>
2023-06-10 22:30:21 +0900

    update ToDo追加して一覧に動的更新

--- todo.js
+++ todo.js
@@ -1,21 +1,10 @@
 class ToDoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    const tempLocalStorageTodoItems = localStorage.getItem("todoItems");
-
-    if (tempLocalStorageTodoItems === null) {
-      this.state = { todoItems: [] };
-    } else {
-      this.state = { todoItems: JSON.parse(tempLocalStorageTodoItems) };
-    }
-  }
-
   render() {
     return (
       <div className="ToDoApp">
         <h1>ToDoアプリ</h1>
-        <AddTodo todoItems={this.state.todoItems} />
-        {/* <TodoTable todoItems={this.props.todoItems} /> */}
+        <AddTodo />
+        <TodoTable />
       </div>
     );
   }
@@ -24,8 +13,18 @@ class ToDoApp extends React.Component {
 class AddTodo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { todo: "", todoItems: this.props.todoItems };
-    this.cnt = 0;
+    const tempLocalStorageTodoItems = localStorage.getItem("todoItems");
+
+    if (tempLocalStorageTodoItems === null) {
+      this.state = { todo: "", todoItems: [] };
+      this.count = 0;
+    } else {
+      this.state = {
+        todo: "",
+        todoItems: JSON.parse(tempLocalStorageTodoItems),
+      };
+      this.count = localStorage.getItem("maxId");
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -37,13 +36,14 @@ class AddTodo extends React.Component {
 
   handleSubmit(event) {
     this.state.todoItems.push({
-      id: this.cnt++,
+      id: this.count++,
       text: this.state.todo,
       // editingText: this.newTodoText,
       // isEditing: false,
     });
 
     localStorage.setItem("todoItems", JSON.stringify(this.state.todoItems));
+    localStorage.setItem("maxId", this.count);
     this.setState({ todo: "" });
     event.preventDefault();
   }
@@ -68,19 +68,19 @@ class AddTodo extends React.Component {
 class TodoTable extends React.Component {
   constructor(props) {
     super(props);
-    this.todos = this.props.todoItems;
+    this.state = { todoItems: [] };
   }
+
   render() {
+    const tempLocalStorageTodoItems = localStorage.getItem("todoItems");
+    if (tempLocalStorageTodoItems !== null)
+      this.setState({ todoItems: JSON.parse(tempLocalStorageTodoItems) });
+
     return (
       <div className="TodoTable">
         <h2>ToDo一覧</h2>
-        {/* <ul>
-          {this.todos.map((todo, index) => {
-            return todo.content;
-          })}
-        </ul> */}
         <ul>
-          {this.todos.map((todo) => (
+          {this.state.todoItems.map((todo) => (
             <li key={todo.id}>{todo.text}</li>
           ))}
         </ul>
@@ -88,9 +88,6 @@ class TodoTable extends React.Component {
     );
   }
 }
-
-const tempLocalStorageTodoItems = localStorage.getItem("todoItems");
-const listItems = JSON.parse(tempLocalStorageTodoItems);
 
 const todoApp = ReactDOM.createRoot(document.getElementById("todoList"));
 todoApp.render(<ToDoApp />);
